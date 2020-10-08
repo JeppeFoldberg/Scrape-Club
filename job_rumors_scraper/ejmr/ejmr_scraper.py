ae9ac11deb584d02dabbed975df19de8f2276718
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2020-10-08 18:15:19 +0200

    update
    changed thread urls function to return duct

--- job_rumors_scraper/ejmr/ejmr_scraper.py
+++ job_rumors_scraper/ejmr/ejmr_scraper.py
@@ -5,12 +5,13 @@ import pandas as pd
 from tqdm import tqdm
 import re
 import pickle
+import datetime
+import math
 
 
 
 
-
-def ejmr_page_urls(from_page, to_page, sub_forum = 'not_specified'):
+def page_urls(from_page, to_page, sub_forum = 'not_specified'):
     """
     ejmr_page_urls is used to gather the URL's for pages of threads on econjobrumors.com. 
     Arguments: From page (Page number to start gathering from), to page and optionally a sub-forum name.
@@ -43,50 +44,111 @@ def ejmr_page_urls(from_page, to_page, sub_forum = 'not_specified'):
     return page_urls
    
 
-def ejmr_thread_urls(page_url_list):
+def thread_urls(page_url_list):
     """
     ejmr_thread_urls is used to gather the URL's threads on econjobrumors.com. 
     Argument: Indivudual page url or list of page urls extracted with ejmr_page_urls.
-    Returns: A list of thread URL's.
+    Returns: A dictionary of thread URL's as key and the page count as value.
     """ 
     # if just one url, turn to list
     if not isinstance(page_url_list, list):
         page_url_list = [page_url_list]
 
     # loop over urls list of thread page responses
-    responses_list = [requests.get(url) for url in page_url_list]
+    responses_list = [requests.get(url) for url in tqdm(page_url_list)]
 
     # loop over responses to create a list of soup objects
     soup_list = [BeautifulSoup(response.text, 'html.parser') for response in responses_list]
 
     # list to store thread content url
     thread_urls = []
+    pages_count = []
 
     for page in soup_list:
         # find body content
         page = page.find('body')
 
-        #loop over thread href
+        #loop over threads
         for thread in page.find_all('tr'):
 
             # regex to extract urls only containing topic
             regex = '(?:https?|ftp)://[\w-]+(?:\.[\w-]+)+/(?:[\w.,@^=%&:/~+-]*/)?topic/(?:[\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?'
-            # extract thread urls
-            thread = re.findall(regex, str(thread))
-            
-            if thread:
-                thread_urls.append(thread[0]) # extract first elemt. Second element is next page in same thread
+            thread_url = re.findall(regex, str(thread)) # extract thread urls
+            if thread_url:
+                thread_urls.append(thread_url[0]) # extract first elemt. Second element is next page in same thread
 
-    return thread_urls
+            # get page count
+            page_count = thread.find('td', {'class':'num'})
+            if page_count:
+                page_count = math.ceil(int(page_count.text) / 20) # there are 20 posts per thread page
+                pages_count.append(page_count) 
 
-def ejmr_page_content(page_url_list, store_locally = 0):#1 for yes
+    # create dictionary with url and page_count as
+    thread_url_dict = dict(zip(thread_urls, pages_count))
+
+
+
+    return thread_url_dict
+
+def dump_page_content(page_url_list, store_locally = 0): #1 for yes
     """
-    Extracts content (thread_title, posts, views, votes etc) from ejmr page.
+    Retrieves and if asked dumps full page content from ejmr page.
     Argument: Indivudual page url or list of page urls extracted with ejmr_page_urls, if the ouput should be stored locally.
-    Returns: List with html responses. Each element is one page. If stored locally - dumps pickle file.
+    Returns: List with soup object. Each element is one page. If stored locally - dumps pickle file.
     """
+    # check if list or single url
+    if not isinstance(page_url_list, list):
+        page_url_list = [page_url_list]
+    
+    # loop over urls list of thread page responses
+    responses_list = [requests.get(url) for url in tqdm(page_url_list)]
 
+    # loop over responses and retrieve soup object
+    #soup_list = [BeautifulSoup(response.text, 'html.parser') for response in responses_list]
+
+    # dump if stored locally
+    if store_locally == 1: 
+        #file name format    
+        file_name = 'ejhr_' + str(len(page_url_list)) + '_pages_' + str(datetime.date.today())
+        
+        with open(file_name, 'wb') as fp: 
+                pickle.dump(responses_list, fp)
+
+    return responses_list 
+
+def dump_thread_content(thread_url_dict, store_locally = 0): #1 for yes
+    """
+    Retrieves and if asked dumps thread content from ejmr thread.
+    Argument: Indivudual thread url or list of thread urls extracted with ejmr_thread_urls, if the ouput should be stored locally.
+    Returns: List with soup object. Each element is one page. If stored locally - dumps pickle file.
+    """
+    
+    soup_list = [BeautifulSoup(thread_url.text, 'html.parser') for thread in tqdm(thread_url_list)]
+
+
+    
+    thread_list = []
+
+
+
+def page_df(page_responses_list):
+    """
+    Retrieves and if asked dumps full page content (thread_title, posts, views, votes etc) from ejmr page.
+    Argument: Indivudual page url or list of page urls extracted with ejmr_page_urls, if the ouput should be stored locally.
+    Returns: List with soup object. Each element is one page. If stored locally - dumps pickle file.
+    """
     pass
+
+
+def thread_df(thread_responses_list):
+    """
+    Retrieves and if asked dumps full page content (thread_title, posts, views, votes etc) from ejmr page.
+    Argument: Indivudual page url or list of page urls extracted with ejmr_page_urls, if the ouput should be stored locally.
+    Returns: List with soup object. Each element is one page. If stored locally - dumps pickle file.
+    """
+    pass
+
+
 
 
 
